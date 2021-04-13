30451171eb58c0341ea7cfee42f97091d11c140d
Haley Billson <h_billson@fanshaweonline.ca>
2021-04-13 01:57:37 -0400

    added youtube API

--- routes/api.js
+++ routes/api.js
@@ -15,150 +15,176 @@ router.get("/users", (req, res) => {
     res.json({message: "all users route"});
 })
 
-router.get("/media/:version/:type/", async function(req, res) {
-    let type = req.params.type;
-    let version = req.params.version;
-    var list = await loadMedia();
-    newitems = list;
-    res.json(newitems);
+router.get("/videos/:q/", async function(req, res) {
+    let q = `${req.params.q} Trailer 1080p`;
+    console.log(q);
 
-    async function loadMedia() {
-            let k = 0;
-            let url ="https://imdb8.p.rapidapi.com/title/get-best-picture-winners";
-            if(type == "tv") {
-                url = "https://imdb8.p.rapidapi.com/title/get-most-popular-tv-shows?homeCountry=US&purchaseCountry=US&currentCountry=US";
-            }
+    let url = `https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${q}`;
+    var response = await fetch(url, {
+        "method": "GET",
+        "headers": {	"x-rapidapi-key": "6d810c5c17mshac4a8e74973b5f5p170738jsn1558e894e034",
+		"x-rapidapi-host": "youtube-search-results.p.rapidapi.com"}
+    }) 
+    .then(response => response.json())
+    .then (data => getID(data))
+    .catch((err) => console.log(err));
+    res.json(await response);
+
+    async function getID(item)  {
+        let id = item["items"][1]["id"];
+        let url = `https://www.youtube.com/embed/${id}`
+        return url;
+    }
+})
+
+        router.get("/media/:version/:type/:decade/", async function(req, res) {
+            let decade = req.params.decade 
+            let type = req.params.type;
+            let version = req.params.version;
+            let items = {};
+            let i = 0;
             let headers = {
                 "x-rapidapi-key": "6d810c5c17mshac4a8e74973b5f5p170738jsn1558e894e034",
                 "x-rapidapi-host": "imdb8.p.rapidapi.com"
             }
-        
-            var response = await fetch(url, {
-                "method": "GET",
-                "headers": headers
-            }) 
-            .then(response => response.json())
-            .then(data => getID(data))
-             .catch((err) => console.log(err));
+            var list = await loadMedia();
+           // console.log(currentitems);
+            console.log(list);
+            res.json(list);
 
-            //console.log(await response);
-            return await response;
+            // res.json(filtereditems);
 
-    
-           async function getID(list) {
-                //getting the details for each title'
-                var deets ={}
-                var currentitems = {};
-                k = 0;
-                if(type == "movies") {
-                    for (var i = 0; i < 80; i++) {
-                        let movie_id = list[i];
-                        movie_id = movie_id.substring(7);
-                        movie_id = movie_id.substring(0, movie_id.length-1);
-                        deets = await getDetails(movie_id);
-                        if(deets) {
-                            currentitems[k] = deets;
-                            k = k+1;
-                        }
-                    } 
-  
-                    return currentitems;
-                } else if (type == "tv") {
-                    currentitems = {};
-                    for (var i = 0; i < 100; i++) {
-                        let show_id = list[i];
-                        show_id = show_id.substring(7);
-                        show_id = show_id.substring(0, show_id.length-1);
-                        deets = await getDetails(show_id);
-                        if(deets) {
-                            currentitems[k] = deets;
-                            k = k+1;
-                        }
-                    }
-                    return currentitems;
-                }
-             }
-    
-             async function getDetails(item) {
-                 let url = `http://www.omdbapi.com/?i=${item}&apikey=7ca22fdc&`;
-                     var response = await fetch(url, {
+                // cut start here
+
+            async function loadMedia() {
+                console.log(`loading ${version} ${type} ${decade}`);
+                    let k = 0;
+                    let url = "";
+                    if(type == "tv") {
+                        url = "https://imdb8.p.rapidapi.com/title/get-most-popular-tv-shows?homeCountry=US&purchaseCountry=US&currentCountry=US";
+                    } else if (type == "movies") { 
+                        url ="https://imdb8.p.rapidapi.com/title/get-best-picture-winners";
+                    }                
+                    var response = await fetch(url, {
                         "method": "GET",
                         "headers": headers
                     }) 
                     .then(response => response.json())
-                    .then(data => filterVersion(data))
-                     .catch((err) => console.log(err));
+                    .then(data => getID(data))
+                    .catch((err) => console.log(err));
 
+                    //console.log(await response);
                     return await response;
-            }
-    
-        async function filterVersion(item) {
-           var list = checkRated(item);
-          // return list;
 
-           function checkRated(item) {
-                let kidSafe = false;
-                    if(item.Rated == "G" || item.Rated =="PG" || item.Rated == "PG-13" || item.Rated == "TV-PG" || item.Rated == "TV-G" || item.Rated == "TV-Y") {
-                        kidSafe = true;
-                    } else {
-                        kidSafe = false;
+                async function getID(list) {
+                        //getting the details for each title'
+                        var deets ={}
+                        var currentitems = {};
+                        
+                        k = 0;
+                        if(type == "movies") {
+                            for (var i = 0; i < 80; i++) {
+                                let movie_id = list[i];
+                                movie_id = movie_id.substring(7);
+                                movie_id = movie_id.substring(0, movie_id.length-1);
+                                deets = await getDetails(movie_id);
+                                if(deets) {
+                                    currentitems[k] = deets;
+                                    k = k+1;
+                                }
+                            } 
+
+                            return currentitems;
+                        } else if (type == "tv") {
+                            currentitems = {};
+                            for (var i = 0; i < 100; i++) {
+                                let show_id = list[i];
+                                show_id = show_id.substring(7);
+                                show_id = show_id.substring(0, show_id.length-1);
+                                deets = await getDetails(show_id);
+                                if(deets) {
+                                    currentitems[k] = deets;
+                                    k = k+1;
+                                }
+                            }
+                            return currentitems;
+                        }
                     }
-                    if(version == "parents") {
-                        return item;
+
+                    async function getDetails(item) {
+                        let url = `http://www.omdbapi.com/?i=${item}&apikey=7ca22fdc&`;
+                            var response = await fetch(url, {
+                                "method": "GET",
+                                "headers": headers
+                            }) 
+                            .then(response => response.json())
+                            .then(data => filterVersion(data))
+                            .catch((err) => console.log(err));
+
+                            return await response;
                     }
-                    else if(version == "kids" && kidSafe) 
-                    {   
-                        return item;
-                } 
 
-           }
-            return await list;
-        }}})
-
-        router.get("/media/:version/:type/:decade/", async function(req, res) {
-            let decade = req.params.decade 
-            console.log(decade);
-             var newrange = await addRanges();
-             console.log(newrange);
-             filtereditems = await filterDecade(newrange)
-             res.json(filtereditems);
-
-            function addRanges() {
-                let range = {
-                    "max_year": "0",
-                    "min_year": "0"
-                };
-                if(decade == "nineties") {
-                    range.max_year = "1999";
-                    range.min_year = "1990";
-                } else if (decade == "eighties") {
-                    range.max_year = "1989";
-                    range.min_year = "1980";
-                } else if (decade == "seventies") {
-                    range.max_year = "1979";
-                    range.min_year = "1970";
-                } else if (decade == "sixties") {
-                    range.max_year = "1969";
-                    range.min_year = "1960"; 
-                } else if (decade == "fifties") {
-                    range.max_year = "1959";
-                    range.min_year = "1950"; 
-                }
-                return range;
-
-            }
-            function filterDecade(range) {
-                let len = Object.entries(newitems).length;
-                let items = {};
-                for (let i = 0;i < len; i++) {
-                    console.log(range);
-                    console.log(newitems[i].Year);
-                    if(newitems[i].Year >= range.min_year && newitems[i].Year <= range.max_year) {
-                        items[i] = newitems[i];
+                async function filterVersion(item) {
+                    var filtered = await checkRated(item);
+                    if(filtered == "no") {
+                       // console.log("not selected");
                     }
-                }
-               return items;
-            }
+                    else {
+                        return await filterDecade(filtered);
+                    }
+                   async function filterDecade(item) {
+                       //console.log(item);
+                        let range = {
+                            "max_year": "0",
+                            "min_year": "0"
+                        };
+                        if(decade == "nineties") {
+                            range.max_year = "1999";
+                            range.min_year = "1990";
+                        } else if (decade == "eighties") {
+                            range.max_year = "1989";
+                            range.min_year = "1980";
+                        } else if (decade == "seventies") {
+                            range.max_year = "1979";
+                            range.min_year = "1970";
+                        } else if (decade == "sixties") {
+                            range.max_year = "1969";
+                            range.min_year = "1960"; 
+                        } else if (decade == "fifties") {
+                            range.max_year = "1959";
+                            range.min_year = "1950"; 
+                        }
+                        if(item.Year >= range.min_year && item.Year <= range.max_year) {
+                             return item
+                        }
+                                           //     let len = Object.entries(newitems).length;
+                    //     let items = {};
+                    //     for (let i = 0;i < len; i++) {
+                    //         if(newitems[i].Year >= range.min_year && newitems[i].Year <= range.max_year) {
+                    //             items[i] = newitems[i];
+                    //         }
+                    //     }
+                    //    return items;
+                    }
+                // return list;
+                async function checkRated(item) {
+                    let filtereditem = {};
+
+                        if (version == "parents") {
+                            filtereditem = item;
+
+                        } else if (version == "kids") {
+                            if(item.Rated == "G" || item.Rated =="PG" || item.Rated == "PG-13" || item.Rated == "TV-PG" || item.Rated == "TV-G" || item.Rated == "TV-Y") {
+                                filtereditem = item;
+                            } else {
+                                filtereditem = "no";
+                            }
+                        }
+                        return filtereditem;
+                    }                
+                }}
+             // cut here 
+
         })
 
 
